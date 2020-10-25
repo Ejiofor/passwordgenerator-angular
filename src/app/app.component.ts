6aa9fcb318ea921aae52a69440e62e6077f734ab
Ejiofor <nwofor@ymail.com>
2020-10-25 12:35:02 +0100

    (passwordgenerator-component):coding the basics of the app

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  password = '';
+  includeLetters = false;
+  
+  getPassword(){
+    return this.password;
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onButtonClick(){
+    console.log(this.includeLetters);
+    this.password = 'MY PASSWORD !!!!';
+  }
+}
